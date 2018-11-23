b4765f45c682538e997dc5dd8887a6ffefff9714
mahdi laanaia <mahdi.laanaia@gmail.com>
2018-11-23 11:34:33 +0100

    Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying database..."
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+# puts "Seeding..."
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+
+# Cocktail.create(name: "Mojito")
+# Cocktail.create(name: "Moscomioul")
+# puts "Seeding done !"
+
+
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+
+
+Cocktail.create(name: "Gin Tonic")
